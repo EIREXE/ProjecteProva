649bfd249c723c8db7321f5ae8b6abee3500824f
Álex Román Núñez <eirexe123@gmail.com>
2019-11-28 16:57:38 +0100

    Añadir tests

---
+++ JavaAppUwU/test/javaappuwu/JavaAppUwUTest.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaappuwu;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author eirexe
+ */
+public class JavaAppUwUTest {
+    
+    public JavaAppUwUTest() {
+    }
+    
+    @BeforeAll
+    public static void setUpClass() {
+    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+    }
+    
+    @AfterEach
+    public void tearDown() {
+    }
+
+    /**
+     * Test of main method, of class JavaAppUwU.
+     */
+    @Test
+    public void testMain() {
+        System.out.println("main");
+        String[] args = null;
+        JavaAppUwU.main(args);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of imprimirHolaMon method, of class JavaAppUwU.
+     */
+    @Test
+    public void testImprimirHolaMon() {
+        System.out.println("imprimirHolaMon");
+        JavaAppUwU.imprimirHolaMon();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of imprimirAltreCosa method, of class JavaAppUwU.
+     */
+    @Test
+    public void testImprimirAltreCosa() {
+        System.out.println("imprimirAltreCosa");
+        JavaAppUwU.imprimirAltreCosa();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
